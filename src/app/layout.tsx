f3334991c03ba8083fcbb25d3003d2c5fe4ea179
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2023-12-08 01:33:11 +0800

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+      <SpeedInsights />
+      </body>
 
       <Script
         id="heap-analytics"
